7aa525830ab8dd1ce9a95f5dcdc73f6aa4e772e8
Fatemeh Farmahini <m_farmahini@yahoo.com>
2017-02-22 15:20:23 +0100

    working with result database

---
+++ js/classes/result-list.class.js
@@ -0,0 +1,23 @@
+class ResultList extends List {
+
+	constructor(items) {
+		super(Result, items);
+	}
+
+	readAllResult(callback) {
+		this.db.readAllResult((data) => {
+			this.push.apply(this, data);
+			console.log(data);
+			callback();
+		});
+	}
+
+	static get sqlQueries() {
+		return {
+			readAllResult: `
+        SELECT id, text FROM grade
+      `
+		}
+	}
+
+}
