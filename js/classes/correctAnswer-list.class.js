737554ca3fbd0fb60b34f5440bae596ea036f1d7
Filip <tjo>
2017-03-01 11:15:30 +0100

    added function to call insert grade calculated grade

--- js/classes/correctAnswer-list.class.js
+++ js/classes/correctAnswer-list.class.js
@@ -1,20 +1,26 @@
 class CorrectAnswerList extends List {
 
-    constructor(items) {
-        super(Question, items);
-    }
+	constructor(items) {
+		super(Question, items);
+	}
 
-    static get sqlQueries() {
-        return {
-            autoCorrect: `
-    	SELECT *
-FROM user_answers_alternative
-JOIN alternative
-ON alternative_optionId = alternativeId
-WHERE user_userId = 'bob@student.com' AND correct = 1
-      `
-        }
+	readCorrectAnswers(callback) {
+		this.db.autoCorrect((data) => {
+			this.push.apply(this, data);
+			callback();
+		});
+	}
 
+	static get sqlQueries() {
+		return {
+			autoCorrect: `
+				SELECT *
+				FROM user_answers_alternative
+				JOIN alternative
+				ON alternative_optionId = alternativeId
+				WHERE user_userId = 'bob@student.com' AND correct = 1
+			`
+		}
 
-    }
+	}
 }
--- js/classes/test.class.js
+++ js/classes/test.class.js
@@ -1,88 +1,108 @@
 class Test extends Base {
 
-    constructor(propertyValues) {
-        super(propertyValues);
+	constructor(propertyValues) {
+		super(propertyValues);
 
-        $("#content").empty();
-        var questionList = new QuestionList();
-        this.questionList = questionList;
-        var alternativeList = new AlternativeList();
-        this.alternativeList = alternativeList;
+		$("#content").empty();
+		var questionList = new QuestionList();
+		this.questionList = questionList;
+		var alternativeList = new AlternativeList();
+		this.alternativeList = alternativeList;
 
-        this.currentQuestion = 0;
-        this.answers = [];
+		this.currentQuestion = 0;
+		this.answers = [];
 
-        var checkGrade = new UserList();
-        checkGrade.checkGrade(() => {
-            if (checkGrade.length > 0) {
-                var userDenied = new UserDenied();
-                userDenied.display('#content');
-            } else {
-                questionList.readAllQuestions(() => {
-                    for (var question of this.questionList) {
-                        question.nbrOfQuestions = this.questionList.length;
-                        this.answers.push(1);
-                    }
+		var checkGrade = new UserList();
+		checkGrade.checkGrade(() => {
+			if (checkGrade.length > 0) {
+				var userDenied = new UserDenied();
+				userDenied.display('#content');
+			} else {
+				questionList.readAllQuestions(() => {
+					for (var question of this.questionList) {
+						question.nbrOfQuestions = this.questionList.length;
+						this.answers.push(1);
+					}
 
-                    alternativeList.readAllAlternatives(() => {
-                        this.showQuestion();
-                    });
+					alternativeList.readAllAlternatives(() => {
+						this.showQuestion();
+					});
 
-                });
+				});
 
-            }
-        });
+			}
+		});
 
-    }
+	}
 
-    insertAnswers(callback) {
-        for (let i = 0; i < this.answers.length; i++) {
-            this.db.insertAnswer({
-                user_userId: window.user,
-                alternative_optionId: this.answers[i]
-            }, callback);
-        }
-    }
+	insertAnswers(callback) {
+		for (let i = 0; i < this.answers.length; i++) {
+			this.db.insertAnswer({
+				user_userId: window.user,
+				alternative_optionId: this.answers[i]
+			}, callback);
+		}
+	}
 
-    showQuestion() {
-        $('#content').empty();
-        this.questionList[this.currentQuestion].display('#content');
+	showQuestion() {
+		$('#content').empty();
+		this.questionList[this.currentQuestion].display('#content');
 
-        for (var alternative of this.alternativeList) {
-            if (alternative.question_questionId === this.questionList[this.currentQuestion].questionId) {
-                alternative.display('#content');
-            }
-        }
+		for (var alternative of this.alternativeList) {
+			if (alternative.question_questionId === this.questionList[this.currentQuestion].questionId) {
+				alternative.display('#content');
+			}
+		}
 
-        var buttons = new Buttons();
-        buttons.test = this;
-        buttons.display('#content');
-    }
+		var buttons = new Buttons();
+		buttons.test = this;
+		buttons.display('#content');
+	}
 
-    autoCorrect() {
-        var autoCorr = new CorrectAnswerList();
-        
-        autoCorr.autoCorrect(() => {
-            console.log(autoCorr);
-        });
-    
-    }
+	autoCorrect() {
+		var autoCorr = new CorrectAnswerList();
 
-    static get sqlQueries() {
-        return {
-            insertAnswer: `
-    	INSERT user_answers_alternative SET ?
-  	
-      `,
-            autoCorrect: `
-    	SELECT *
-FROM user_answers_alternative
-JOIN alternative
-ON alternative_optionId = alternativeId
-WHERE user_userId = 'bob@student.com' AND correct = 1
-      `
-        }
+		autoCorr.readCorrectAnswers(() => {
+			var g = Math.floor(this.questionList.length * 0.5);
+			var vg = Math.floor(this.questionList.length * 0.75);
+			var grade;
 
+			console.log('g', g);
+			console.log('vg', vg);
+			console.log('AutoCorr length:', autoCorr.length);
 
-    }
+			if (autoCorr.length < g) {
+				console.log('if 1');
+				grade = 'ig';
+			} else if (autoCorr.length >= g && autoCorr.length < vg) {
+				console.log('if 2');
+				grade = 'g';
+			} else {
+				console.log('if 3');
+				grade = 'vg';
+			}
+
+			this.insertGrade(grade);
+		});
+	}
+
+	insertGrade(grade, callback) {
+		this.db.insertGrade({
+			user_userId: window.user,
+			grade: grade
+		}, callback);
+	}
+
+	static get sqlQueries() {
+		return {
+			insertAnswer: `
+    			INSERT user_answers_alternative SET ?
+     		`,
+			insertGrade: `
+    			INSERT grade SET ?
+     		`
+
+		}
+
+	}
 }
