5b03757b13f31f64436097fd3176d73c8ab2de5d
Filip <tjo>
2017-03-01 13:05:04 +0100

    nbr of IG, G, VG statistics in result

--- js/classes/result-view.class.js
+++ js/classes/result-view.class.js
@@ -1,17 +1,38 @@
 class ResultView extends Base {
 
-  constructor(propertyValues){
-    super(propertyValues);
-     $("#content").empty();
+	constructor(propertyValues) {
+		super(propertyValues);
+		$("#content").empty();
 
 		var resultList = new ResultList();
 
-				resultList.readAllResult(() => {
-					console.log("Read from DB", resultList);
-					resultList.display('#content');
-				});
+		resultList.readAllResult(() => {
+			console.log("Read from DB", resultList);
+			resultList.display('#content');
 
-		
-}
+			var ig = 0;
+			var g = 0;
+			var vg = 0;
+
+			for (var result of resultList) {
+				console.log('Grade', result);
+				if (result.grade == 'ig') {
+					ig++;
+				} else if (result.grade == 'g') {
+					g++;
+				} else {
+					vg++;
+				}
+			}
+			
+			$('#content').append('<p>IG: ' + ig + '</p>');
+			$('#content').append('<p>G: ' + g + '</p>');
+			$('#content').append('<p>VG: ' + vg + '</p>');
+
+			$('#content').append('<p>Average: ska in sen här ja</p>');
+			$('#content').append('<p>Tid: Tid ska in på varje rad för varje användare</p>');
+		});
+
+	}
 
 }
