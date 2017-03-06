cc4d85404e5cb10e0616f96ab8783a9a2935e9e8
Filip <tjo>
2017-03-06 10:11:37 +0100

    removed global variable for showResult Changed "Average" to "Average Points"

--- js/main.js
+++ js/main.js
@@ -3,22 +3,19 @@
 /* Users */
 
 /* Class 1 */
-var user = 'david@student.com';
+//var user = 'david@student.com';
 //var user = 'leo@student.com';
 
 /* Class 2 */
-//var user = 'filip@student.com';
+var user = 'filip@student.com';
 //var user = 'tobias@student.com';
 //var user = 'fatemeh@student.com';
 
-/* Teahers */
-//var user = 'john@teacher.com';	// Class 1
+/* Teachers */
+//var user = 'john@teacher.com'	;	// Class 1
 //var user = 'jane@teacher.com'; // Class 2
 
 var student;
-
-/* Set if show result immediately to the student */
-//var showResult = true;
 
 $(() => {
 	new App()
