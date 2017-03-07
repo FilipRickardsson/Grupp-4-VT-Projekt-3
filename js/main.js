46106f2f0ab6c538fb50fabf6fe37aaede60699c
Filip <tjo>
2017-03-07 14:22:08 +0100

    added files for submitting project

--- js/main.js
+++ js/main.js
@@ -3,17 +3,17 @@
 /* Users */
 
 /* Class 1 */
-var user = 'david@student.com';
+//var user = 'david@student.com';
 //var user = 'leo@student.com';
 
 /* Class 2 */
-//var user = 'filip@student.com';
+var user = 'filip@student.com';
 //var user = 'tobias@student.com';
 //var user = 'fatemeh@student.com';
 
 /* Teachers */
 //var user = 'john@teacher.com';	// Class 1
-//var user = 'jane@teacher.com'; // Class 2
+var user = 'jane@teacher.com'; // Class 2
 
 var student;
 
